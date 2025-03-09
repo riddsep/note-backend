1ff9992371968d7ff378a03fa5147ca606bc8c2f
riddsep <ridoseptiawan00@gmail.com>
2025-03-10 06:30:45 +0700

    add user models

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,8 +1,9 @@
 const notesRouter = require("express").Router();
 const Note = require("../models/note");
+const User = require("../models/user");
 
 notesRouter.get("/", async (request, response) => {
-  const notes = await Note.find({});
+  const notes = await Note.find({}).populate("user", { username: 1, name: 1 });
   response.json(notes);
 });
 
@@ -22,13 +23,19 @@ notesRouter.get("/:id", async (request, response, next) => {
 notesRouter.post("/", async (request, response, next) => {
   const body = request.body;
 
+  const user = await User.findById(body.userId);
+
   const note = new Note({
     content: body.content,
     important: body.important || false,
+    user: user.id,
   });
 
   try {
     const savedNote = await note.save();
+    user.notes.push(savedNote._id);
+    await user.save();
+
     response.status(201).json(savedNote);
   } catch (exception) {
     next(exception);
